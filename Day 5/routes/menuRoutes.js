45ae55ff57590ccb4e502f928fe3f08b217fc708
inderjeetbhagat <62701663+inderjeetbhagat@users.noreply.github.com>
2024-03-22 23:46:25 +0530

    Day 6 Assignment part 2

--- Day 5/routes/menuRoutes.js
+++ Day 5/routes/menuRoutes.js
@@ -26,18 +26,29 @@ router.get('/', async (req,res) =>{
         res.status(500).json({error: 'Internal server error'});
     }
 })
-router.get('/:tasteType', async (req,res) =>{
+
+
+router.put('/:id', async(req,res) =>{
     try{
-    const tasteType = req.params.tasteType;
-    const response = await MenuItem.find({taste: tasteType})
-    console.log("Data Fetched");
-    res.status(200).json(response);
-    } catch(err){
-        console.log(err);
-        res.status(500).json({error: 'Internal taste'});
+        const menuId = req.params.id;
+        const updatedMenuData = req.body;
+        const response = await MenuItem.findByIdAndUpdate(menuId,updatedMenuData,{
+            new: true,
+            runValidators:true
+        })
+
+        if(!response){
+            return res.status(404).json({error: 'Menu Item not found'});
+        }
+        console.log('Data Updated');
+        res.status(200).json(response);
+
+
+    }catch(err) {
+console.log(err);
+res.status(500).json({error: 'Internal Server Error'});
+
     }
 })
-
-router.get('/menu/:taste')
 
 module.exports = router;
